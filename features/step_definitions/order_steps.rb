82d88b448ab8bc76beed72f287b7d619edcd63c3
Bo Liang <rbaggio018@gmail.com>
2013-12-27 09:44:20 -0500

    Cleanup cucumber scenarios

--- features/step_definitions/order_steps.rb
+++ features/step_definitions/order_steps.rb
@@ -1,25 +1,3 @@
-When(/^(.*) makes an order (.*) which has price \$(.*)$/) do |user, order, price|
-  visit root_url
-  fill_in 'Order',  with: order
-  fill_in 'Price',  with: price
-  fill_in 'Name',   with: user
-  click_button 'Place'
-end
-
-Then(/^(.*) which has price \$(.*) under (.*) should be in the order list$/) do |order, price, user|
-  visit orders_url
-  page.should have_content order
-  page.should have_content price
-  page.should have_content user
-end
-
-Then(/^(.*) which has price \$(.*) under (.*) should not be in the order list$/) do |order, price, user|
-  visit orders_url
-  page.should_not have_content order
-  page.should_not have_content price
-  page.should_not have_content user
-end
-
 When(/^(.*) makes an order (.*) with (.*) which has price \$(.*)$/) do |user, order, specs, price|
   visit root_url
   fill_in 'Order',  with: order
@@ -35,4 +13,12 @@ Then(/^(.*) with (.*) which has price \$(.*) under (.*) should be in the order l
   page.should have_content specs
   page.should have_content price
   page.should have_content user
+end
+
+Then(/^(.*) with (.*) which has price \$(.*) under (.*) should not be in the order list$/) do |order, specs, price, user|
+  visit orders_url
+  page.should_not have_content order
+  page.should_not have_content specs
+  page.should_not have_content price
+  page.should_not have_content user
 end
