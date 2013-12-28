4309276f4579412936707b7f1cc454569fafc5a5
Bo Liang <rbaggio018@gmail.com>
2013-12-27 23:17:03 -0500

    Validate user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ActiveRecord::Base
   has_many :group_orders, foreign_key: 'customer_id'
   # has_many :transactions
 
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
+  validates :balance, presence: true, numericality: true
+
   def add_balance(amount)
     update_attributes(balance: balance + amount)
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,64 @@ describe User do
   let(:user) { FactoryGirl.build(:user) }
   subject { user }
 
+  it { should be_valid }
+
+  describe 'validations' do
+
+    context 'without a name' do
+      before { user.name = "" }
+
+      it { should_not be_valid }
+    end
+
+    context 'with duplicate name' do
+      before { FactoryGirl.create(:user, name: user.name.upcase) }
+
+      it { should_not be_valid }
+    end
+
+    context 'without a balance' do
+      before { user.balance = "" }
+
+      it { should_not be_valid }
+    end
+
+    context 'with no numerical balance' do
+      before { user.balance = "text" }
+
+      it { should_not be_valid }
+    end
+  end
+
+  describe 'initial balance' do
+
+    context 'when not specified' do
+
+      it 'sets to 0.0' do
+        user.save
+        expect(user.reload.balance).to eq(0.0)
+      end
+    end
+
+    context 'when specified' do
+      before { user.balance = 6.75 }
+
+      it 'does not set to default' do
+        user.save
+        expect(user.reload.balance).to eq(6.75)
+      end
+    end
+
+    context 'when precision is more than 2' do
+      before { user.balance = 6.666 }
+
+      it 'rounds to 2' do
+        user.save
+        expect(user.reload.balance).to eq(6.67)
+      end
+    end
+  end
+
   describe '#add_balance' do
 
     context 'possitive amount' do
