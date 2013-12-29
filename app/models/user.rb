b9905d4450c8b592b4b39241aa5a31abdc176ae7
Bo Liang <rbaggio018@gmail.com>
2013-12-29 01:08:17 -0500

    Add devise and google-oauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, omniauth_providers: [:google_oauth2]
+
+  # attr_accessible :provider, :uid
 
   has_many :orders, foreign_key: 'customer_id'
   has_many :group_orders, foreign_key: 'customer_id'
@@ -13,5 +20,19 @@ class User < ActiveRecord::Base
 
   def add_balance(amount)
     update_attributes!(balance: balance + amount)
+  end
+
+  def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
+    data = access_token.info
+    user = User.where(:email => data["email"]).first
+
+    unless user
+      user = User.create(
+        name: data["name"],
+        email: data["email"],
+        password: Devise.friendly_token[0,20]
+      )
+    end
+    user
   end
 end
