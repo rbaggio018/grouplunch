8c8536e3527cb165c36c7567f74567f95c88c425
Bo Liang <rbaggio018@gmail.com>
2014-01-05 02:37:04 -0500

    Style transaction pages

--- features/step_definitions/transaction_steps.rb
+++ features/step_definitions/transaction_steps.rb
@@ -4,5 +4,5 @@ When(/^(.*) receives a transaction of \$(.*) from (.*)$/) do |user, amount, user
   visit new_transaction_path
   fill_in 'From', with: User.where(name: user_from).first.email
   fill_in 'Amount', with: amount
-  click_button 'Transfer'
+  click_button 'Receive'
 end
